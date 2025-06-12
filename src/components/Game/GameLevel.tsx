5174b803c5dd64b54d5dd333c169a7fc8aa3103c
Henry Hoang <tuanhm2703@gmail.com>
2025-06-12 16:00:49 +0700

    update

--- src/components/Game/GameLevel.tsx
+++ src/components/Game/GameLevel.tsx
@@ -6,9 +6,12 @@ import { LEVEL_MAX } from "../../types/game";
 const GameLevel = () => {
   const { t } = useTranslation();
   const { level } = useRecoilValue(gameState);
+  const currentLevel = Number.parseInt(level) > Number.parseInt(LEVEL_MAX) ? LEVEL_MAX : level
   return (
     <h3 className="game-title" style={{ fontSize: '1.3rem', color: '#D22128', boxShadow: 'none' }}>
-      {t("Level")} <span style={{ color: '#D22128', background: '#fff', padding: '4px', borderRadius: '15px', boxShadow: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px' }}>{level > LEVEL_MAX ? LEVEL_MAX : level} / {LEVEL_MAX}</span>
+      {t("Level")} <span style={{ color: '#D22128', background: '#fff', padding: '4px', borderRadius: '15px', boxShadow: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px' }}>
+        {currentLevel} / {LEVEL_MAX}
+        </span>
     </h3>
   );
 };
